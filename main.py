6e2001ac3252e08065ed90cb408357764b314345
MatorMirne <mirnemator@gmail.com>
2023-01-29 21:52:37 +0900

    slack 명령어 듣기 성공

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import uvicorn
+from secret import Token
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token=Token.SLACK_APP_TOKEN)
+
+@app.command("/chatops")
+def handle_some_command(ack, body, logger):
+    ack()
+    logger.info(body)
+    print(body)
 
 if __name__ == "__main__":
-    print('a')
+    SocketModeHandler(app, Token.SLACK_APP_TOKEN).start()
